7f10c7dc122eedfb0be52184a08fcfdb31dd5cc9
dfpaglia <dfpaglia@mtu.edu>
2021-01-10 13:16:24 -0500

    Attempted to add depth frame print out, need to try something new on system

--- src/SARndbox-2.7/VolumeTool.h
+++ src/SARndbox-2.7/VolumeTool.h
@@ -9,14 +9,24 @@
 /* Forward declarations: */
 class Sandbox;
 class VolumeTool;
+class FrameFilter;
 typedef Vrui::GenericToolFactory<VolumeTool> VolumeToolFactory;
+typedef Kinect::FrameSource::DepthCorrection::PixelCorrection PixelDepthCorrection; // Type for per-pixel depth correction factors
 
 class VolumeTool:public Vrui::Tool,public Vrui::Application::Tool<Sandbox>
 	{
 	friend class Vrui::GenericToolFactory<VolumeTool>;
+
+	unsigned int frameSize[2]; // Width and height of the camera's depth frames
+	PixelDepthCorrection* pixelDepthCorrection; // Buffer of per-pixel depth correction coefficients
+	Kinect::FrameSource::IntrinsicParameters cameraIps; // Intrinsic parameters of the Kinect camera
+	FrameFilter* frameFilter; // Processing object to filter raw depth frames from the Kinect camera
+	Threads::TripleBuffer<Kinect::FrameBuffer> filteredFrames; // Triple buffer for incoming filtered depth frames
+	DepthImageRenderer* depthImageRenderer; // Object managing the current filtered depth image
 	
 	/* Elements: */
 	private:
+	void receiveFilteredFrame(const Kinect::FrameBuffer& frameBuffer); // Callback receiving filtered depth frames from the filter object
 	static VolumeToolFactory* factory; // Pointer to the factory object for this class
 	
 	/* Constructors and destructors: */
